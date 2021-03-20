f57b11efe4940a891ecdf86c58b75c3b76bdc7a5
michelle22cerbo <cerbomichelle129@gmail.com>
2021-03-20 20:09:53 +0800

    javac
    this has method inside

---
+++ cerbo.java
@@ -0,0 +1,11 @@
+public class Main {
+
+  static void myMethod() {
+    System.out.println("CASE STUDY!");
+  }
+ static void myName() {
+    System.out.println("Michelle");
+  }
+ static void myLname() {
+    System.out.println("Cerbo");
+  }
