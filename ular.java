aadac0604128a25564449863c5bc4b3d8ed5fad9
shaine02 <shaineyu15@gmail.com>
2021-03-20 23:10:39 +0800

    contributor

---
+++ ular.java
@@ -0,0 +1,19 @@
+public class Main {
+
+ static void myPname() {
+    System.out.println("Shaine");
+  }
+
+ static void myPlname() {
+    System.out.println("Ular");
+  }
+ static void myCourse() {
+    System.out.println("Info Tech");
+  }
+  public static void main(String[] args) {
+	myPname();
+	myPlname();
+	myCourse();
+
+  }
+}
